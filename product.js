3900247dc3251c7767417e7936fe6ceeaf2b6bbd
onigirisumu <76844268+onigirisumu@users.noreply.github.com>
2024-11-12 15:31:17 +0600

    Update product.js

--- product.js
+++ product.js
@@ -1,7 +1,8 @@
 async function getWeather() {
     const apiKey = '087f9351949d4f85a1d91223241211';
     const city = 'London'; 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=087f9351949d4f85a1d91223241211&units=metric`;
+
 
     try {
         const response = await fetch(url);
